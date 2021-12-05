bcee84ed30e2ea946f2d16a813b43f1f384b6b1d
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-12-04 19:49:19 -0500

    Second Commit

---
+++ FoodDeliveryAppMVC/Controllers/FoodController.cs
@@ -0,0 +1,20 @@
+﻿using fooddeliveryapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodDeliveryAppMVC.Controllers
+{
+    [Authorize]
+    public class FoodController : Controller
+    {
+        // GET: Food
+        public ActionResult Index()
+        {
+            var model = new FoodListItem[0];
+            return View(model);
+        }
+    }
+}
---
+++ fooddeliveryapp.Models/FoodListItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fooddeliveryapp.Models
+{
+   public class FoodListItem
+    {
+
+        [Display(Name = "Food Name")]
+        public string FoodName { get; set; }
+        [Required]
+        [Display(Name = "Food Price")]
+        public int FoodPrice { get; set; }
+        [Required]
+        [Display(Name = "Food Ingridients")]
+        public string FoodIngridients { get; set; }
+    }
+}
