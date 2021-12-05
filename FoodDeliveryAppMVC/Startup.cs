760517052b087143b5b1f3d3dbe7f830947660d6
Amel MallemLouda <amelmalleml2016@gmail.com>
2021-12-04 19:18:12 -0500

    Add project files.

---
+++ FoodDeliveryAppMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodDeliveryAppMVC.Startup))]
+namespace FoodDeliveryAppMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ fooddeliveryapp.Data/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fooddeliveryapp.Data
+{
+    public class Food
+    {
+        [Key]
+        public int FoodId { get; set; }
+        [Required]
+        [Display(Name = "Food Name")]
+        public string FoodName { get; set; }
+        [Required]
+        [Display(Name = "Food Price")]
+        public int FoodPrice { get; set; }
+        [Required]
+        [Display(Name = "Food Ingridients")]
+        public string FoodIngridients { get; set; }
+    }
+}
